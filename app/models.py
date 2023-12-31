290e11b176c1732850d3c1e9afa3360b539ba520
iam-Akashgiri <047a.giri@gmail.com>
2023-12-31 11:29:12 +0530

    first try to deploy

--- app/models.py
+++ app/models.py
@@ -25,9 +25,9 @@ class Food(models.Model):
 class Customer(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    phone = models.IntegerField()
+    phone = models.BigIntegerField()
     email = models.EmailField()
-    password = models.CharField(max_length=50)
+    password = models.CharField(max_length=1000)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-4hr(s#w1$4qm%%q^qme+(yfdq&@_cq+aqn)-8e_#ds3)g)452-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'C21bfe-b4*f15geaB5FdEgdaGaFDAFFG',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '48204',
     }
 }
 
@@ -121,9 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
